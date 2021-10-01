70604698cd98950171a3435d2754eb19741e950d
Emmy Le <emilytuyetle@gmail.com>
2021-09-30 23:31:25 -0700

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Person 2",
+    imgURL:
+      "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/question-mark.png",
+    phone: "+123 456 789",
+    email: "person2@gmail.com"
+  },
+  {
+    name: "Person 3",
+    imgURL:
+      "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/question-mark.png",
+    phone: "+123 456 789",
+    email: "person3@gmail.com"
+  }
+];
+
+export default contacts;
